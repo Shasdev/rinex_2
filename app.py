2f0fc23631f4a45b940a6e8bb5a9b9be1e46931c
M Shashank Dev Padakannaya <99595131+Shasdev@users.noreply.github.com>
2023-09-22 00:13:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+import os
+os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
+import streamlit as st
+import joblib
+
 import streamlit as st
 import joblib
 model=joblib.load('spam-ham')
